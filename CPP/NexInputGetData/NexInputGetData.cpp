6db1b8720ab8194fd2dc4989d079d9f6b0039564
r57zone <r57zone@gmail.com>
2020-06-01 00:00:55 +0400

    Improvements & headers in a self file

--- CPP/NexInputGetData/NexInputGetData.cpp
+++ CPP/NexInputGetData/NexInputGetData.cpp
@@ -1,86 +1,17 @@
 ﻿#include <iostream>
 #include <atlstr.h>
+#include "NexInput.h"
 
-#define NEX_GAMEPAD_DPAD_UP				0x0001
-#define NEX_GAMEPAD_DPAD_DOWN			0x0002
-#define NEX_GAMEPAD_DPAD_LEFT			0x0004
-#define NEX_GAMEPAD_DPAD_RIGHT			0x0008
-#define NEX_GAMEPAD_START				0x0010
-#define NEX_GAMEPAD_BACK				0x0020
-#define NEX_GAMEPAD_LEFT_THUMB			0x0040
-#define NEX_GAMEPAD_RIGHT_THUMB			0x0080
-#define NEX_GAMEPAD_LEFT_SHOULDER		0x0100
-#define NEX_GAMEPAD_RIGHT_SHOULDER		0x0200
-#define NEX_GAMEPAD_A					0x1000
-#define NEX_GAMEPAD_B					0x2000
-#define NEX_GAMEPAD_X					0x4000
-#define NEX_GAMEPAD_Y					0x8000
+HMODULE hDll;
+_NEXInputGetState NEXInputGetState;
+_NEXInputSetState NEXInputSetState;
+_NEXInputGetInfo NEXInputGetInfo;
+_NEXInputPowerOff NEXInputPowerOff;
+NEX_INPUT_STATE NexControllerInputState;
+NEX_OUTPUT_STATE NexControllerOutputState;
+NEX_CONTROLLER_INFO NexControllerInfo;
 
-#define NEX_CONTROLLER_WIRED			0
-#define NEX_CONTROLLER_WIRELESS			1
-
-#define NEX_INPUT_MAX_COUNT				4
-
-#define ERROR_DEVICE_NOT_CONNECTED		1
-#define ERROR_SUCCESS					0
-
-#define NEX_UNKNOWN_CONTROLLER			0
-
-#define MICROSOFT_XBOX_360_CONTROLLER	1
-#define MICROSOFT_XBOX_ONE_CONTROLLER	2
-
-#define SONY_DUALSHOCK_3_CONTROLLER		26
-#define SONY_DUALSHOCK_4_CONTROLLER		27
-#define SONY_DUALSHOCK_5_CONTROLLER		28
-
-#define NINTENDO_SWITCH_PRO_CONTROLLER	51
-
-typedef struct _NEX_INPUT_STATE
-{
-	WORD								Buttons;
-	BYTE								LeftTrigger;
-	BYTE								RightTrigger;
-	SHORT								AxisLX;
-	SHORT								AxisLY;
-	SHORT								AxisRX;
-	SHORT								AxisRY;
-	float								Yaw;
-	float								Pitch;
-	float								Roll;
-} NEX_INPUT_STATE, *PNEX_INPUT_STATE;
-
-typedef struct _NEX_OUTPUT_STATE
-{
-	WORD								LeftMotorSpeed;
-	WORD								RightMotorSpeed;
-	BYTE								LEDBrightness;
-	BYTE								LEDRed;
-	BYTE								LEDGreen;
-	BYTE								LEDBlue;
-} NEX_OUTPUT_STATE, *PNEX_OUTPUT_STATE;
-
-typedef struct _NEX_CONTROLLER_INFO
-{
-	WORD								ControllerType;
-	BYTE								ConnectType;
-	BYTE								BatteryLevel;
-	bool								SupportRotation;
-} NEX_CONTROLLER_INFO, *PNEX_CONTROLLER_INFO;
-
-typedef DWORD(__stdcall *_NEXInputGetState)(__in DWORD dwUserIndex, __out NEX_INPUT_STATE *pInputState);
-typedef DWORD(__stdcall *_NEXInputSetState)(__in DWORD dwUserIndex, __in NEX_OUTPUT_STATE *pOutputState);
-typedef DWORD(__stdcall *_NEXInputGetInfo)(__in DWORD dwUserIndex, __out NEX_CONTROLLER_INFO *pControllerInfo);
-typedef DWORD(__stdcall *_NEXInputPowerOff)(__in DWORD dwUserIndex);
-
-int main()
-{
-	SetConsoleTitle(_T("NexInput Get Data"));
-	HMODULE hDll;
-	_NEXInputGetState NEXInputGetState;
-	_NEXInputSetState NEXInputSetState;
-	_NEXInputGetInfo NEXInputGetInfo;
-	_NEXInputPowerOff NEXInputPowerOff;
-
+int NexInputInit() {
 	CRegKey key;
 	TCHAR NexLibDll[MAX_PATH];
 
@@ -89,10 +20,10 @@ int main()
 	{
 		ULONG regSize = sizeof(NexLibDll);
 
-		#ifdef _WIN64
-			status = key.QueryStringValue(_T("NexInput_v1.0_64"), NexLibDll, &regSize);
-		#else
-			status = key.QueryStringValue(_T("NexInput_v1.0_32"), NexLibDll, &regSize);
+		#if _M_IX86
+				status = key.QueryStringValue(_T("NexInput_v1.0_32"), NexLibDll, &regSize);
+		#elif _WIN64
+				status = key.QueryStringValue(_T("NexInput_v1.0_64"), NexLibDll, &regSize);
 		#endif
 	}
 	key.Close();
@@ -105,84 +36,124 @@ int main()
 		NEXInputPowerOff = (_NEXInputPowerOff)GetProcAddress(hDll, "NEXInputPowerOff");
 
 		if (NEXInputGetState == NULL)
-			printf(" NEXInputGetState not found\r\n");
+			printf(" NEXInputGetState not found\n");
 		if (NEXInputSetState == NULL)
-			printf(" NEXInputSetState not found\r\n");
+			printf(" NEXInputSetState not found\n");
 		if (NEXInputGetInfo == NULL)
-			printf(" NEXInputGetInfo not found\r\n");
+			printf(" NEXInputGetInfo not found\n");
 		if (NEXInputPowerOff == NULL)
-			printf(" NEXInputPowerOff not found\r\n");
-	} 
-	else
-	{
-		printf(" NexInput standard not found.\r\n");
+			printf(" NEXInputPowerOff not found\n");
+		return 0;
+	} else {
+		printf(" NexInput standard not found.\n");
+		if (!PathFileExists(NexLibDll))
+			printf(" Dll not found.\n");
+		return 1;
+	}
+}
+
+void NexInputFree() {
+	NEXInputGetState = NULL;
+	NEXInputSetState = NULL;
+	NEXInputGetInfo = NULL;
+	NEXInputPowerOff = NULL;
+
+	if (hDll != NULL) {
+		FreeLibrary(hDll);
+		hDll = nullptr;
+	}
+
+	//CoFreeUnusedLibraries();
+}
+
+int main()
+{
+	SetConsoleTitle(_T("NexInput Get Data"));
+	
+	bool DebugMode = false;
+
+	NexInputInit();
+
+	if (NexInputInit() == 1) {
 		system("pause");
 		return 1;
 	}
 
-	NEX_INPUT_STATE NexControllerInputState;
-	NEX_OUTPUT_STATE NexControllerOutputState;
-	NEX_CONTROLLER_INFO NexControllerInfo;
-
 	while (true) {
 		system("cls");
 
+		if ((GetAsyncKeyState('R') & 0x8000) != 0) {
+			NexInputFree();
+			hDll = NULL;
+			NexInputInit();
+			printf("\n Restart gamepads\n");
+			Sleep(300);
+			continue;
+		}
+
 		for (int i = 0; i < NEX_INPUT_MAX_COUNT; i++) {
-			if (NEXInputGetState(i, &NexControllerInputState) == ERROR_DEVICE_NOT_CONNECTED)
+			if (NEXInputGetState == NULL || NEXInputGetState(i, &NexControllerInputState) == ERROR_DEVICE_NOT_CONNECTED)
 				continue;
 
-
-			printf("\r\n Gamepad index = %d, name = ", i);
+			printf("\n Gamepad index = %d, name = ", i);
 
 			NEXInputGetInfo(i, &NexControllerInfo);
 			switch (NexControllerInfo.ControllerType) {
-			case MICROSOFT_XBOX_360_CONTROLLER:
+				case MICROSOFT_XBOX_CONTROLLER:
 				{
-					printf("MICROSOFT_XBOX_360_CONTROLLER \r\n");
+					printf("MICROSOFT XBOX CONTROLLER\n");
+					break;
+				}
+				case MICROSOFT_XBOX_360_CONTROLLER:
+				{
+					printf("MICROSOFT XBOX 360 CONTROLLER\n");
 					break;
 				}
 				case MICROSOFT_XBOX_ONE_CONTROLLER:
 				{
-					printf("MICROSOFT_XBOX_ONE_CONTROLLER \r\n");
+					printf("MICROSOFT XBOX ONE CONTROLLER\n");
 					break;
 				}
 				case SONY_DUALSHOCK_3_CONTROLLER:
 				{
-					printf("SONY_DUALSHOCK_3_CONTROLLER \r\n");
+					printf("SONY DUALSHOCK 3 CONTROLLER\n");
 					break;
 				}
 				case SONY_DUALSHOCK_4_CONTROLLER:
 				{
-					printf("SONY_DUALSHOCK_4_CONTROLLER \r\n");
+					printf("SONY DUALSHOCK 4 CONTROLLER\n");
 					break;
 				}
 				case SONY_DUALSHOCK_5_CONTROLLER:
 				{
-					printf("SONY_DUALSHOCK_5_CONTROLLER \r\n");
+					printf("SONY DUALSHOCK 5 CONTROLLER\n");
 					break;
 				}
 				case NINTENDO_SWITCH_PRO_CONTROLLER:
 				{
-					printf("NINTENDO_SWITCH_PRO_CONTROLLER \r\n");
+					printf("NINTENDO SWITCH PRO CONTROLLER\n");
 					break;
 				}
 
 				default:
 				{
-					printf("NEX_UNKNOWN_CONTROLLER \r\n");
+					printf("NEX UNKNOWN CONTROLLER\n");
 					break;
 				}
 			}
 
 			
 			if (NexControllerInfo.SupportRotation)
-				printf(" Gyroscope support, Yaw=%7.2f, Pitch=%7.2f, Roll=%7.2f\r\n", NexControllerInputState.Yaw, NexControllerInputState.Pitch, NexControllerInputState.Roll);
+				printf(" Gyroscope support, Yaw=%7.2f, Pitch=%7.2f, Roll=%7.2f\n", NexControllerInputState.Yaw, NexControllerInputState.Pitch, NexControllerInputState.Roll);
 			else
-				printf(" Gyroscope unsupport \r\n");
-			printf(" AxisLX=%d, AxisLY=%d, AxisRX=%d, AxisRY=%d\r\n", NexControllerInputState.AxisLX, NexControllerInputState.AxisLY, NexControllerInputState.AxisRX, NexControllerInputState.AxisRY);
-			printf(" LeftTrigger=%d, RightTrigger=%d\r\n", NexControllerInputState.LeftTrigger, NexControllerInputState.RightTrigger);
+				printf(" Gyroscope unsupport \n");
+			printf(" AxisLX=%d, AxisLY=%d, AxisRX=%d, AxisRY=%d\n", NexControllerInputState.AxisLX, NexControllerInputState.AxisLY, NexControllerInputState.AxisRX, NexControllerInputState.AxisRY);
+			printf(" LeftTrigger=%d, RightTrigger=%d\n", NexControllerInputState.LeftTrigger, NexControllerInputState.RightTrigger);
 
 			printf(" Buttons pressed: ");
+			if (DebugMode)
+				printf("(%d)", NexControllerInputState.Buttons);
+
 			if (NexControllerInputState.Buttons & NEX_GAMEPAD_DPAD_UP) printf("DPAD UP ");
 			if (NexControllerInputState.Buttons & NEX_GAMEPAD_DPAD_LEFT) printf("DPAD LEFT ");
 			if (NexControllerInputState.Buttons & NEX_GAMEPAD_DPAD_RIGHT) printf("DPAD RIGHT ");
@@ -202,14 +173,14 @@ int main()
 			if (NexControllerInputState.Buttons & NEX_GAMEPAD_X) printf("X ");
 			if (NexControllerInputState.Buttons & NEX_GAMEPAD_Y) printf("Y ");
 
-			if (NexControllerInputState.Buttons == 0) printf("-");
-
+			if (NexControllerInputState.Buttons == 0 && DebugMode == false) printf("-");
+			
 
 			NEXInputGetInfo(i, &NexControllerInfo);
 			if (NexControllerInfo.ConnectType == NEX_CONTROLLER_WIRELESS)
-				printf("\r\n Controller type: %d; connection: wireless, battery capacity: %d.\r\n", NexControllerInfo.ControllerType, NexControllerInfo.BatteryLevel);
+				printf("\n Controller type: %d; connection: wireless, battery capacity: %d.\n", NexControllerInfo.ControllerType, NexControllerInfo.BatteryLevel);
 			else
-				printf("\r\n Controller type: %d, connection: wired, battery capacity: %d.\r\n", NexControllerInfo.ControllerType, NexControllerInfo.BatteryLevel);
+				printf("\n Controller type: %d, connection: wired, battery capacity: %d.\n", NexControllerInfo.ControllerType, NexControllerInfo.BatteryLevel);
 		}
 
 		if ((GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0) {
@@ -223,14 +194,14 @@ int main()
 				NEXInputSetState(i, &NexControllerOutputState);
 		}
 		if ((GetAsyncKeyState(VK_ESCAPE) & 0x8000) != 0) break;
+		if ((GetAsyncKeyState('D') & 0x8000) != 0) DebugMode = true;
 
-		printf("\r\n Ctrl - feedback (vibration and LED), Escape - exit.\r\n");
+		printf("\n Ctrl - feedback (vibration and LED), Escape - exit.\n");
 
 		Sleep(50);
 	}
 
-	if (hDll != NULL)
-		FreeLibrary(hDll);
+	NexInputFree();
 
 	return 0;
 }
